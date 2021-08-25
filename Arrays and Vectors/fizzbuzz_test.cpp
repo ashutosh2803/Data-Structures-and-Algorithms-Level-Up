8ceeb9ca9ff2f81d9a371ac7fbd41ea133eda9fe
unknown <ashuisalive@gmail.com>
2021-08-25 22:06:22 +0530

    arrays and vectors added

---
+++ Arrays and Vectors/fizzbuzz_test.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
+
+vector<string> fizzbuzz(int n){
+    vector<string> result;
+    for (int i = 1; i <= n; i++){
+        if((i % 3) == 0){
+            if(i % 5 == 0){
+                result.push_back("FizzBuzz");
+                continue;
+            }
+            result.push_back("Fizz");
+        }
+        else if((i % 5) == 0){
+            result.push_back("Buzz");
+        }
+        else{
+            result.push_back(to_string(i));
+        }
+    }
+    return result;
+}
+
+int main(){
+    vector<string> result = fizzbuzz(20);
+    cout << result << endl;
+    return 0;
+}
